f86345af82079bb2d58763cec7d251d2000a6489
akomzolov <akomzolov.mail@yandex.ru>
2024-12-26 21:45:40 +0300

    PageObject

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class mainPage(BasePage): 
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
