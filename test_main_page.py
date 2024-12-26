58380c82fe3d307bdfc6453bc9c9dbbe0be14f92
akomzolov <akomzolov.mail@yandex.ru>
2024-12-26 22:04:44 +0300

    Debug

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,6 @@ from pages.main_page import MainPageTest
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPageTest(browser, link)
     page.open()
     page.should_be_login_link()
