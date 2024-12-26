e8f3baf5fae5eb319dcd79b0840c3ca83143b9b0
akomzolov <akomzolov.mail@yandex.ru>
2024-12-26 21:56:03 +0300

    Page Object Debug

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-class BasePage():
+class BasePageTest():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
-from pages.base_page import BasePage
+from .base_page import BasePageTest
 from selenium.webdriver.common.by import By
 
-class mainPage(BasePage): 
+class MainPageTest(BasePageTest): 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
