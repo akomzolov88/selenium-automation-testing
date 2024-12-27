a2403976dadd633757550500e49a73c9f1f920c8
akomzolov <akomzolov.mail@yandex.ru>
2024-12-27 08:51:35 +0300

    Debug

--- page_object/conftest.py
+++ page_object/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from selenium import webdriver
 
-def pytest_adoption(parser):
-    parser.adoption("--language", action="store", default="en", help="Language for the browser")
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en", help="Language for the browser")
 
 @pytest.fixture(scope="function")
 def browser(request):
