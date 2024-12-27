c643f29f0725ffad0121ef909a89df46541a0a30
akomzolov <akomzolov.mail@yandex.ru>
2024-12-27 08:48:22 +0300

    Debuf

--- page_object/conftest.py
+++ page_object/conftest.py
@@ -1,14 +1,14 @@
 import pytest
 from selenium import webdriver
 
-def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default="en", help="Language for the browser")
+def pytest_adoption(parser):
+    parser.adoption("--language", action="store", default="en", help="Language for the browser")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    options.add_experimental_option('pref', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
--- page_object/test_main_page.py
+++ page_object/test_main_page.py
@@ -2,9 +2,12 @@ from pages.main_page import MainPageTest
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPageTest(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page = MainPageTest(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина                      
+    page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
